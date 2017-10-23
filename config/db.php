7b64d56c49d306e167eba3e0d38064c0fd946c2e
Serhii Kosyouk <monstrpro@gmail.com>
2017-10-23 11:33:02 +0300

    Add admin CRUD

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii_basic_blog',
     'username' => 'root',
-    'password' => '',
+    'password' => 'qqq',
     'charset' => 'utf8',
 ];
---
+++ models/Article.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property string $content
+ * @property string $date
+ * @property string $image
+ * @property integer $viewed
+ * @property integer $user_id
+ * @property integer $status
+ * @property integer $category_id
+ *
+ * @property ArticleTag[] $articleTags
+ * @property Comment[] $comments
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['description', 'content'], 'string'],
+            [['date'], 'safe'],
+            [['viewed', 'user_id', 'status', 'category_id'], 'integer'],
+            [['title', 'image'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'viewed' => 'Viewed',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticleTags()
+    {
+        return $this->hasMany(ArticleTag::className(), ['article_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['article_id' => 'id']);
+    }
+}
